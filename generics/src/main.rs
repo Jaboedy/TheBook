dee73188b97677eb498574b9d485fff92ee8c209
Brody Underwood <tbunde01@gmail.com>
2024-12-26 22:39:10 -0500

    Generics and Traits: Add trait examples

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Display};
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
@@ -9,6 +11,7 @@ fn main() {
 
     let _int_point = Point { _x: 5, _y: 10 };
     let _float_point = Point { _x: 1.0, _y: 4.0 };
+    summary_test();
 }
 
 // abstract the functionality to avoid duplication
@@ -63,3 +66,133 @@ enum _Result<T, E> {
     Ok(T),
     Err(E),
 }
+
+// Traits are similar to a feature called interfaces
+// in many other languages
+pub trait Summary {
+    fn summarize(&self) -> String;
+
+    // or default behaviors can be provided
+    fn summarize_with_default(&self) -> String {
+        String::from("(Read more...)")
+    }
+    // these methods with defaults can call other
+    // methods defined in the trait
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn summary_test() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("Of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+// To specify a function should accept a parameter
+// only if it is a type that implements a trait
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// This can also be written as follows
+pub fn notify2<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// To implement more than one required
+// trait you can use this syntax
+pub fn notify3(item: &(impl Summary + Display)) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify4<T: Summary + Display>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Trait bounds can get complicated, where clauses
+// can help clarify this
+pub fn some_function<T: Display + Clone, U: Clone + Debug>(_t: &T, _u: &U) -> i32 {
+    println!("Demo");
+    1
+}
+
+pub fn some_alternative_function<T, U>(_t: &T, _u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    println!("Demo");
+    1
+}
+
+// We can also use the impl Trait syntax to specify
+// a return type.  This syntax doesn't work if
+// multiple types could be returned depending on a condition
+// in the function.  This case will be covered later
+pub fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("name"),
+        content: String::from("content"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// You can also use trait bounds to implement
+// methods on a type only if it implements other
+// traits
+struct _Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> _Pair<T> {
+    fn _new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> _Pair<T> {
+    fn _cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+// We can also conditionally impl a Trait for any type that
+// implements another trait.  This is called a blanket implementation
+// impl<T: Display> ToString for T {
+//    // --snip--
+// }
